a64a520459d34aed5f41e52ebf9e21d74c9128cf
FlareFlo <mail@flareflo.dev>
2023-06-30 17:15:06 +0200

    Color table and exclude side dishes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use cli_table::{Cell, print_stdout, Table};
+use cli_table::{Cell, Color, ColorChoice, print_stdout, Style, Table};
 use cli_table::format::Justify;
 use futures::future::join_all;
 use serde::{Deserialize, Serialize};
@@ -10,6 +10,8 @@ const MENSA_2: (usize, &str) = (105, "Mensa 2");
 const MENSA_360: (usize, &str) = (111, "360°");
 
 const TO_FETCH: &[(usize, &str)] = &[MENSA_1, MENSA_2, MENSA_360];
+
+const LOWER_PRICE_THRESHOLD: f64 = 2.0; // Do not display things cheaper than this
 
 const BASE_DOMAIN: &str = "https://sls.api.stw-on.de";
 
@@ -48,18 +50,34 @@ async fn main() {
 	for i in TO_FETCH {
 		threads.push(get_todays_menu(i.0));
 	}
-	let menus = join_all(threads).await.into_iter().zip(TO_FETCH.iter());
+	let menus = join_all(threads).await
+		.into_iter()
+		.zip(TO_FETCH.iter());
 
 	for (menu, (_, mensa_name)) in menus {
+		let cell_color = Some(Color::Green);
 		let table = menu.meals
-			.into_iter()
-			.map(|meal| vec![meal.name.cell(), meal.price.student.cell().justify(Justify::Right)])
+			.iter()
+			.filter(|meal|meal.price.student > LOWER_PRICE_THRESHOLD)
+			.map(|meal|
+				vec![
+					meal.name.as_str().cell().foreground_color(cell_color),
+					meal.price.student.cell().justify(Justify::Right).foreground_color(cell_color)
+				])
 			.collect::<Vec<_>>()
 			.table()
 			.title(vec![
-                format!("{mensa_name}").as_str(),
-                "Price",
-            ]);
+                format!("{mensa_name} (excluding {} menu items/side dishes cheaper than {LOWER_PRICE_THRESHOLD}€)",
+						menu.meals.iter()
+							.filter(|meal|meal.price.student <= LOWER_PRICE_THRESHOLD)
+							.count()
+				).as_str()
+					.cell()
+					.foreground_color(Some(Color::Cyan)),
+                "Price".cell()
+					.foreground_color(Some(Color::Cyan)),
+            ])
+			.color_choice(ColorChoice::Auto);
 
 		print_stdout(table).unwrap();
 	}
