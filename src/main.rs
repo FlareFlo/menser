f568e57326e5eb97cd65685bbda1ebabc34a4872
FlareFlo <mail@flareflo.dev>
2023-07-03 16:19:26 +0200

    Coloring for prices

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use cli_table::{Cell, Color, ColorChoice, print_stdout, Style, Table};
+use cli_table::Color::Rgb;
 use cli_table::format::Justify;
 use futures::future::join_all;
 use pad::PadStr;
@@ -32,9 +33,23 @@ impl Menu {
 			.len()
 	}
 	pub fn longest_menu_names(menus: &[MenuItem]) -> usize {
-		 menus.iter().max_by(|lhs, rhs|{
+		menus.iter().max_by(|lhs, rhs| {
 			lhs.0.longest_menu_name().cmp(&rhs.0.longest_menu_name())
 		}).unwrap().0.longest_menu_name()
+	}
+
+	pub fn most_expensive_meal(&self) -> f64 {
+		self.meals
+			.iter()
+			.max_by(|lhs, rhs|
+				lhs.price.student.total_cmp(&rhs.price.student)
+			).unwrap()
+			.price.student
+	}
+	pub fn most_expensive_meals(menus: &[MenuItem]) -> f64 {
+		menus.iter().max_by(|lhs, rhs| {
+			lhs.0.most_expensive_meal().total_cmp(&rhs.0.most_expensive_meal())
+		}).unwrap().0.most_expensive_meal()
 	}
 }
 
@@ -64,26 +79,25 @@ async fn request_menu(id: usize) -> Menu {
 
 #[tokio::main]
 async fn main() {
-
 	let menus = fetch_menus().await;
 
 	let longest_meal_name = Menu::longest_menu_names(&menus);
+	let most_expensive_price = Menu::most_expensive_meals(&menus);
 
-
-	let meta = vec![vec!["today".cell().justify(Justify::Center), "".cell()]]
+	let meta = vec![vec!["today".cell(), "".cell()]]
 		.table()
 		.title(vec![
 			"Fetched from".pad_to_width(longest_meal_name).cell().foreground_color(Some(Color::Cyan)),
-			"".pad_to_width(7).cell()
+			"".pad_to_width(7).cell(),
 		]);
 	print_stdout(meta).unwrap();
 
-	render_menus(menus, longest_meal_name);
+	render_menus(menus, longest_meal_name, most_expensive_price);
 }
 
 type MenuItem<'a> = (Menu, &'a (usize, &'a str));
 
-async fn fetch_menus<'a>() ->  Vec<MenuItem<'a>> {
+async fn fetch_menus<'a>() -> Vec<MenuItem<'a>> {
 	let mut threads = vec![];
 	for i in TO_FETCH {
 		threads.push(request_menu(i.0));
@@ -95,23 +109,29 @@ async fn fetch_menus<'a>() ->  Vec<MenuItem<'a>> {
 }
 
 
-fn render_menus<'a>(menus: impl IntoIterator<Item = MenuItem<'a>>, longest_meal_name: usize) {
+fn render_menus<'a>(menus: impl IntoIterator<Item=MenuItem<'a>>, longest_meal_name: usize, most_expensive_price: f64) {
+	let compute_price_color = |price: f64| {
+		let lerp_color = |x: f64|(1.1 * x + 33.0).round() as u8;
+		let lerp_price = |x: f64|(x - LOWER_PRICE_THRESHOLD)  /  (most_expensive_price - LOWER_PRICE_THRESHOLD) * 100.0;
+		Rgb( lerp_color(lerp_price(price)), lerp_color(100.0 - lerp_price(price)), 33)
+	};
+
 	for (menu, (_, mensa_name)) in menus {
 		let cell_color = Some(Color::Green);
 		let table = menu.meals
 			.iter()
-			.filter(|meal|meal.price.student > LOWER_PRICE_THRESHOLD)
+			.filter(|meal| meal.price.student > LOWER_PRICE_THRESHOLD)
 			.map(|meal|
 				vec![
 					meal.name.pad_to_width(longest_meal_name).as_str().cell().foreground_color(cell_color),
-					meal.price.student.cell().justify(Justify::Right).foreground_color(cell_color)
+					meal.price.student.cell().justify(Justify::Right).foreground_color(Some(compute_price_color(meal.price.student))),
 				])
 			.collect::<Vec<_>>()
 			.table()
 			.title(vec![
 				format!("{mensa_name} (excluding {} menu items/side dishes cheaper than {LOWER_PRICE_THRESHOLD}€)",
 						menu.meals.iter()
-							.filter(|meal|meal.price.student <= LOWER_PRICE_THRESHOLD)
+							.filter(|meal| meal.price.student <= LOWER_PRICE_THRESHOLD)
 							.count()
 				).as_str()
 					.cell()
