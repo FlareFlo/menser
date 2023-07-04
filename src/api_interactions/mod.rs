96e3b9109a7876dc862647be7037ad4fe4a42d37
FlareFlo <mail@flareflo.dev>
2023-07-04 12:42:49 +0200

    Speed up sync fetch time by threading

--- src/api_interactions/mod.rs
+++ src/api_interactions/mod.rs
@@ -1,5 +1,6 @@
 use crate::api_schema::MenuItem;
 use crate::constants;
+use crate::constants::TO_FETCH;
 
 #[cfg(feature = "async-reqwest")]
 mod r#async;
@@ -15,20 +16,28 @@ pub fn fetch_menus<'a>() -> Vec<MenuItem<'a>> {
 	{
 		let rt = tokio::runtime::Runtime::new().unwrap();
 		let mut threads = vec![];
-		for i in constants::TO_FETCH {
+		for i in TO_FETCH {
 			threads.push(rt.spawn(crate::api_interactions::r#async::request_menu(i.0)));
 		}
 		let menus = rt.block_on(futures::future::join_all(threads))
 			.into_iter()
 			.map(|future|future.unwrap())
-			.zip(constants::TO_FETCH.iter());
+			.zip(TO_FETCH.iter());
 		rt.shutdown_timeout(std::time::Duration::from_secs(10));
 		menus.collect()
 	}
 	#[cfg(feature = "sync-ureq")]
 	{
-		constants::TO_FETCH.iter()
-			.map(|i|(crate::api_interactions::sync::request_menu(i.0), i))
-			.collect()
+		let mut threads = vec![];
+		for i in constants::TO_FETCH {
+			threads.push(std::thread::spawn(|| {
+				crate::api_interactions::sync::request_menu(i.0)
+			}));
+		}
+		let mut joined = vec![];
+		for thread in threads {
+			joined.push(thread.join().unwrap());
+		}
+		joined.into_iter().zip(TO_FETCH.iter()).collect()
 	}
 }
