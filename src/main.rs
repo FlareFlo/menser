ead4c1a211999befc292dba9bd37b9d250da96cd
FlareFlo <mail@flareflo.dev>
2023-07-20 18:22:22 +0200

    Add ChatGPT integration and refactor main function
    This commit introduces integration with the ChatGPT API and refactors the main function to accommodate these changes. A 'ChatGPT' client is created at the start of the program, using the 'dotenv' for environment variable handling. Additionally, the 'get_menus' logic has been moved into a separate function to improve code structure and readability.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,52 @@
+#![feature(lazy_cell)]
+
 mod api_schema;
 mod constants;
 mod table_formatting;
 mod api_interactions;
 
+use std::cell::{LazyCell, OnceCell};
+use std::env;
+use std::sync::LazyLock;
+use chatgpt::client::ChatGPT;
+use chatgpt::config::ChatGPTEngine::{Gpt35Turbo, Gpt4, Gpt4_32k_0314};
+use chatgpt::config::{ChatGPTEngine, ModelConfigurationBuilder};
+use chatgpt::types::CompletionResponse;
+use dotenv::dotenv;
 use crate::api_interactions::fetch_menus;
-use crate::api_schema::{Menu};
+use crate::api_schema::{Menu, MenuItem};
 use crate::table_formatting::{render_menus, render_meta};
 
-fn main() {
+pub static GPT: LazyLock<ChatGPT> = LazyLock::new(||{
+	// Load env files to env-vars
+	dotenv().ok();
+	let token = env::var("TOKEN").unwrap();
+	let model_config = ModelConfigurationBuilder::default()
+		.engine(Gpt35Turbo)
+		.build()
+		.unwrap();
+
+	let client = ChatGPT::new_with_config(token, model_config).unwrap();
+	client
+});
+
+#[tokio::main]
+async fn main() {
+	let mut convo = GPT.new_conversation_directed("You are glumpy, a tool to assist users in determining the best meal to eat this day");
+	convo.send_message("");
+
+	let (menus, day) = get_menus();
+
+	let longest_meal_name = Menu::longest_menu_names(&menus);
+	let most_expensive_price = Menu::most_expensive_meals(&menus);
+
+	render_meta(longest_meal_name, &day);
+
+	render_menus(menus, longest_meal_name, most_expensive_price);
+}
+
+
+fn get_menus() -> (Vec<MenuItem<'static>>, &'static str) {
 	let current_day = time::OffsetDateTime::now_local().unwrap().weekday().to_string().to_lowercase();
 	let week_days = vec!["monday", "tuesday", "wednesday", "thursday", "friday"]
 		.into_iter()
@@ -27,11 +66,5 @@ fn main() {
 		}
 		menu
 	}.expect("No menus in weekdays at all");
-
-	let longest_meal_name = Menu::longest_menu_names(&menus);
-	let most_expensive_price = Menu::most_expensive_meals(&menus);
-
-	render_meta(longest_meal_name, &day);
-
-	render_menus(menus, longest_meal_name, most_expensive_price);
+	(menus, day)
 }
