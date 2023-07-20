d232fc68014ccde04363fdeebf7f2dbedacbb6cb
FlareFlo <mail@flareflo.dev>
2023-07-20 18:47:04 +0200

    Add functionality for personalized food recommendations
    This update includes changes necessary for our tool to provide meal recommendations based on the user's personal preferences. The main.rs file was adjusted to read a text file listing preferred and disliked foods. Also added is a conversation-direction.txt file that instructs the chatbot on providing meal recommendations. With these changes, our tool can provide more targeted and personalized food suggestions.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod table_formatting;
 mod api_interactions;
 
 use std::cell::{LazyCell, OnceCell};
-use std::env;
+use std::{env, fs};
 use std::sync::LazyLock;
 use chatgpt::client::ChatGPT;
 use chatgpt::config::ChatGPTEngine::{Gpt35Turbo, Gpt4, Gpt4_32k_0314};
@@ -32,22 +32,30 @@ pub static GPT: LazyLock<ChatGPT> = LazyLock::new(||{
 
 #[tokio::main]
 async fn main() {
-	let mut convo = GPT.new_conversation_directed("You are glumpy, a tool to assist users in determining the best meal to eat this day");
-	convo.send_message("");
-
 	let (menus, day) = get_menus();
 
 	let longest_meal_name = Menu::longest_menu_names(&menus);
 	let most_expensive_price = Menu::most_expensive_meals(&menus);
 
-	render_meta(longest_meal_name, &day);
+	// render_meta(longest_meal_name, &day);
 
-	render_menus(menus, longest_meal_name, most_expensive_price);
+	// render_menus(menus.clone(), longest_meal_name, most_expensive_price);
+
+	let personal_preferences = fs::read_to_string("food-preferences.txt").unwrap();
+
+	let mut convo = GPT.new_conversation_directed(include_str!("../gpt-config/conversation-direction.txt"));
+	let query = format!("Following meals are available today: {menus:?}. i would like to eat something that fits my personal-preferences {personal_preferences}");
+	let res = convo.send_message(&query).await.unwrap();
+	println!("{}", &res.message().content);
+	let est_cost = res.usage.total_tokens as f64 / 1000.0 * 0.002;
+	println!("Estimated API cost: {}$", est_cost);
+	println!("Using one dollar, you could make {:.1} requests", 1.0 / est_cost);
 }
 
 
 fn get_menus() -> (Vec<MenuItem<'static>>, &'static str) {
-	let current_day = time::OffsetDateTime::now_local().unwrap().weekday().to_string().to_lowercase();
+	// let current_day = time::OffsetDateTime::now_local().unwrap().weekday().to_string().to_lowercase();
+	let current_day = "thursday".to_owned();
 	let week_days = vec!["monday", "tuesday", "wednesday", "thursday", "friday"]
 		.into_iter()
 		.skip_while(|day|day!= &current_day);
