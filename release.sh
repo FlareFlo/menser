f4dd4243ec32c3bde5cb898d3fc6c559706067bd
FlareFlo <mail@flareflo.dev>
2023-10-31 02:50:15 +0100

    Bad ordering of operations for git tag

--- release.sh
+++ release.sh
@@ -13,11 +13,11 @@ read -p "Automatically add and push tag $version to git? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  git tag $version
   git add Cargo.lock
   git add Cargo.toml
-  git commit -m "Releasing version $version" &&
-  git push &&
+  git commit -m "Releasing version $version"
+  git push
+  git tag $version
   git push origin $version
 fi
 
